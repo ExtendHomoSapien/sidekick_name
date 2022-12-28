6a9e8cec7fec20e9b98a8561bb8c69826ae6d84d
James <jkcarrolliv@gmail.com>
2022-12-27 19:50:01 -0500

    pig latin exercise (jc)

---
+++ pig_latin.py
@@ -0,0 +1,32 @@
+"""Includes logic converting words into pig latin."""
+import random
+import sys
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+
+def main():
+    """Return pig latin version of word until user interrupt."""
+    word = ''
+    print("Welcome to Pig Latin Maker.'\n")
+
+    while True:
+
+        user_word = input("\n\nEnter a word and press enter? (Enter 'n' to quit)\n ")
+        if user_word.lower() == "n":
+            break
+
+        first_letter = user_word[0]
+        suffix = first_letter + 'ay' if first_letter not in vowels else first_letter + 'way'
+        pig_latin_word = user_word[1:] + suffix
+
+        print("\n\n")
+        print(pig_latin_word, file=sys.stderr)
+        print("\n\n")
+
+    input("\nPress Enter to exit.")
+
+
+print(__name__)
+if __name__ == "__main__":
+    main()
