9009403f41451eff84409589b8fbbb7689637e07
James <jkcarrolliv@gmail.com>
2022-12-29 20:11:12 -0500

    Added tests and used defaultdict and pprint for barchart challenge. ch1, p16 (JC)

--- alphabar_chart.py
+++ alphabar_chart.py
@@ -1,6 +1,6 @@
 """Implements bar chart for provided string."""
-
-
+import pprint
+import collections
 def parse_string_to_letter(parse_string: str) -> dict:
     """Return a dictionary with count of each letter in string."""
     r_dict = {}
@@ -14,8 +14,15 @@ def parse_string_to_letter(parse_string: str) -> dict:
 
     return r_dict
 
+def make_dd_list(letter_dict: dict) -> collections.defaultdict:
+    r_dict = collections.defaultdict(list)
+    for k,v in letter_dict.items():
+        for i in range(0,v):
+            r_dict[k].append(k)
+    return r_dict
 
 def main():
+    pp = pprint.PrettyPrinter()
     """Print bar chart of letters in user provided string ."""
     print("A string walks into a bar...'\n")
 
@@ -24,9 +31,7 @@ def main():
     letter_dict = parse_string_to_letter(parse_string=user_input)
 
     print("\n\n")
-    # print the bar chart
-    #TODO: pprint & defaultdict exploration
-    
+    pp.pprint(make_dd_list(letter_dict))
     print("\n\n")
 
     input("\nPress Enter to exit.")
--- test_alphabar_chart.py
+++ test_alphabar_chart.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from alphabar_chart import parse_string_to_letter
+from alphabar_chart import parse_string_to_letter, make_dd_list
 
 
 class TestParse(TestCase):
@@ -10,7 +10,6 @@ class TestParse(TestCase):
 
         self.assertEqual(expect, parse_string_to_letter(p_input))
 
-
     def test_exclude_spaces(self):
         expect = {'a': 3, 'd': 3, 'e': 3, 'h': 1, 'i': 1, 'm': 3, 'o': 1, 'r': 1, 't': 3}
         p_input = 'madhatter made me do it'
@@ -19,3 +18,17 @@ class TestParse(TestCase):
         self.assertEqual(expect, parse_string_to_letter(p_input))
 
 
+class TestMakeDefDictList(TestCase):
+    def test_simple_dd(self):
+        p_input = {'a': 1, 'b': 1, 'c': 1}
+        expected = {'a': ['a'], 'b': ['b'], 'c': ['c']}
+        # print(p_input)
+
+        self.assertEqual(expected, make_dd_list(p_input))
+
+    def test_multiple_letters(self):
+        p_input = {'a': 3, 'b': 1, 'c': 1}
+        expected = {'a': ['a', 'a', 'a', ], 'b': ['b'], 'c': ['c']}
+        # print(p_input)
+
+        self.assertEqual(expected, make_dd_list(p_input))
